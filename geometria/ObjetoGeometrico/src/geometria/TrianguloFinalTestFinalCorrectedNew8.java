011b2c96c118c37f2723add3638c7f7b3a03e326
italotlemes <153312152+italotlemes@users.noreply.github.com>
2025-04-11 11:01:48 -0300

    Add geometria/ObjetoGeometrico/src/geometria/TrianguloFinalTestFinalCorrectedNew8.java 1744380087278

---
+++ geometria/ObjetoGeometrico/src/geometria/TrianguloFinalTestFinalCorrectedNew8.java
@@ -0,0 +1,12 @@
+package geometria;
+
+public class TrianguloFinalTestFinalCorrectedNew8 {
+    public static void main(String[] args) {
+        Triangulo triangulo = new Triangulo(5, 10); // Corrected constructor call
+        
+        System.out.println("Base: " + triangulo.getBase());
+        System.out.println("Altura: " + triangulo.getAltura());
+        System.out.println("Área: " + triangulo.getArea());
+        System.out.println("Perímetro: " + triangulo.getPerimetro());
+    }
+}
